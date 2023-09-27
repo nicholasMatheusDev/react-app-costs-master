c69798481b472d9b6bb728ff63851024588835cd
Nicholas Matheus Dev <146299361+nicholasMatheusDev@users.noreply.github.com>
2023-09-27 20:58:40 -0300

    Add files via upload

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,18 @@
+import styles from './Footer.module.css'
+import { FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa';
+function Footer (){
+    return (
+        <footer className={styles.footer}>
+            <ul className={styles.socialList}>
+                <li><FaFacebook /></li>
+                <li><FaInstagram /></li>
+                <li><FaLinkedin /></li>
+            </ul>
+            <p className={styles.copyRight}>
+                <span>Costs</span> &copy; 2021
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
